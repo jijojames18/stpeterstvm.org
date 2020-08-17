8d6b20cbf900ee70d1899455381626abd13335ac
Jijo James <jijojames18@gmail.com>
2020-08-17 08:56:59 +0530

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
