e3f669caa3022cc97c00a4522b0b32bd7a7ab837
Jijo James <jijojames18@gmail.com>
2020-08-22 08:06:37 +0530

    Service worker added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,3 +13,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
