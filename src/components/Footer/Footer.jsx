ea745d0d4954703b73994eee4ac76e490ec32444
Jijo James <jijojames18@gmail.com>
2020-08-22 14:43:09 +0530

    Footer fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faEnvelopeOpen,
   faPhone,
   faMapMarker,
 } from '@fortawesome/free-solid-svg-icons';
+import fbIcon from '../../assets/fb.svg';
+import youtubeIcon from '../../assets/youtube.svg';
 import './Footer.scss';
 
 export default function Footer() {
@@ -18,26 +20,62 @@ export default function Footer() {
       <Container>
         <Row>
           <Col>
-            <Navbar>
-              <Nav>
-                <h5>Links</h5>
-              </Nav>
-            </Navbar>
-            <Navbar>
-              <Nav>
-                <Nav.Link href="/">Home</Nav.Link>
-              </Nav>
-            </Navbar>
-            <Navbar>
-              <Nav>
-                <Nav.Link href="/about">About</Nav.Link>
-              </Nav>
-            </Navbar>
-            <Navbar>
-              <Nav>
-                <Nav.Link href="/contact">Contact</Nav.Link>
-              </Nav>
-            </Navbar>
+            <Container className="contact-container nav-items">
+              <Row>
+                <Col>
+                  <h5>Links</h5>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Nav.Link href="/">Home</Nav.Link>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Nav.Link href="/about">About</Nav.Link>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Nav.Link href="/contact">Contact</Nav.Link>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col>
+            <Container className="contact-container social-links">
+              <Row>
+                <Col>
+                  <h5>Follow us on</h5>
+                </Col>
+              </Row>
+              <Row className="social-icons">
+                <Col>
+                  <a
+                    href="https://m.facebook.com/profile.php?id=394517177352666"
+                    target="_blank"
+                    title="facebook"
+                    rel="noopener noreferrer"
+                  >
+                    <Image src={fbIcon} alt="Navigate to Facebook page" />
+                  </a>
+                </Col>
+                <Col>
+                  <a
+                    href="https://www.youtube.com/channel/UC_T9Z9EjXSlffJV4M_NDELg"
+                    target="_blank"
+                    title="youtube"
+                    rel="noopener noreferrer"
+                  >
+                    <Image
+                      src={youtubeIcon}
+                      alt="Navigate to Youtube channel"
+                    />
+                  </a>
+                </Col>
+              </Row>
+            </Container>
           </Col>
           <Col>
             <Container className="contact-container">
