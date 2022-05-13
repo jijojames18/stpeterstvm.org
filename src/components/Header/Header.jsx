89339593609d27ffd64bab1776ce17a2e356566c
Jijo James <jijojames18@gmail.com>
2022-05-13 21:10:49 +0530

    Add events page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
         <Nav>
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/events">Current Events</Nav.Link>
+          <Nav.Link href="/events">Events</Nav.Link>
           <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
